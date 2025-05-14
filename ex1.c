c599984625cd57b68df8b7ba1b3fa5be385a9f52
semihkalkandelen <semihkdelen@gmail.com>
2025-05-14 12:27:00 +0300

    s:1-q:1

---
+++ ex1.c
@@ -0,0 +1,11 @@
+//write a program to display following exactly:
+//name: "john"
+//Tabbed\tOutput
+//backslash: //
+
+#include <stdio.h>
+int main(){
+    printf("name: \"john\"\n");
+    printf("tabbed\\tOutput\n");
+    printf("backslash: //\n\n");
+}
